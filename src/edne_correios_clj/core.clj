4020ff7bd87fff48b00f4e028f4973e95890c24a
Allan Gonçalves <allangoncalves@outlook.com>
2025-02-26 16:09:08 -0300

    using correios SQL + csv output

--- src/edne_correios_clj/core.clj
+++ src/edne_correios_clj/core.clj
@@ -3,9 +3,10 @@
             [clojure.string :as str]
             [edne-correios-clj.db :as db]
             [edne-correios-clj.db-schemas :as db-schemas]
+            [clojure.data.csv :as csv]
             [next.jdbc :as jdbc]))
 
-(def ^:dynamic *batch-size* 100)
+(def ^:dynamic *seed-batch-size* 100)
 
 (def log-dir "./src/data/log")
 (def delta-dir "./src/data/delta")
@@ -17,13 +18,6 @@
     ([result input]
      (with-open [y input]
        (rf result y)))))
-
-(defn print-reducer
-  ([]
-   nil)
-  ([_])
-  ([_ input]
-   (println input)))
 
 (defn sum-reducer
   ([]
@@ -73,7 +67,8 @@
                        with-open-xform
                        (mapcat line-seq)
                        (map #(str/split % #"\@"))
-                       (partition-all *batch-size*)
+                       (map (partial replace {"" nil}))
+                       (partition-all *seed-batch-size*)
                        (map (partial db/bulk-insert! table-name)))
                  file-paths))
 
@@ -83,44 +78,43 @@
                        with-open-xform
                        (mapcat line-seq)
                        (map #(str/split % #"\@"))
+                       (map (partial replace {"" nil}))
                        (map (partial delta-op! table-name)))
                  file-paths))
 
+(defn -main []
+  (doseq [[table-name {:keys [columns]}] db-schemas/tables]
+    (db/create-table table-name columns))
+
+  (doseq [[table-name files] (->> (clojure.java.io/file log-dir)
+                                  file-seq
+                                  (map #(.getPath %))
+                                  (group-by find-table-name))
+          :when (some? table-name)]
+    (println table-name)
+    (seed table-name files))
+
+  (doseq [[table-name files] (->> (clojure.java.io/file delta-dir)
+                                  file-seq
+                                  (map #(.getPath %))
+                                  (group-by find-table-name))
+          :when (some? table-name)]
+    (println table-name)
+    (apply-delta-files table-name files))
+
+  (db/create-cep-view)
+
+  (with-open [writer (io/writer "output.csv")]
+    (csv/write-csv writer
+                   (cons ["cep" "endereco" "bairro" "cidade" "uf" "uf_nome"]
+                    (db/fetch-ceps)))))
+
 (comment
 
- (doseq [[table-name {:keys [columns]}] db-schemas/tables]
-   (db/create-table table-name columns))
+ (require '[clj-memory-meter.core :as mm]
+          '[clj-async-profiler.core :as prof])
+ #_(mm/measure (-main))
 
- (doseq [[table-name files] (->> (clojure.java.io/file log-dir)
-                                 file-seq
-                                 (map #(.getPath %))
-                                 (group-by find-table-name))
-         :when (some? table-name)]
-   (println table-name)
-   (seed table-name files))
+ (prof/profile (-main))
 
- (doseq [[table-name files] (->> (clojure.java.io/file delta-dir)
-                                 file-seq
-                                 (map #(.getPath %))
-                                 (group-by find-table-name))
-         :when (some? table-name)]
-   (println table-name)
-   (apply-delta-files table-name files))
-
- (db/create-cep-view)
-
- #_(let [query "SELECT * FROM ceps"
-       result (jdbc/plan ds [query])]
-   (with-open [writer (io/writer "output.csv")]
-     (csv/write-csv writer
-                    (cons (keys (first result))
-                          (map vals result)))))
- )
-
-
-
-
-(defn -main [])
-
-;; Uncomment the following line to run the -main function when executing this script
-;; (-main)
+ #_(prof/serve-ui 8080))
--- src/edne_correios_clj/db.clj
+++ src/edne_correios_clj/db.clj
@@ -1,6 +1,7 @@
 (ns edne-correios-clj.db
   (:require [honey.sql :as sql]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [next.jdbc.plan :as plan]))
 
 (defn debug [el]
   (def el-debug el)
@@ -36,42 +37,166 @@
 
 (def cep-view
   "CREATE VIEW ceps AS
-   SELECT COALESCE(log.cep, loc.cep) AS CEP,
-     CASE log.log_sta_tlo
-      WHEN 'S' THEN
-      COALESCE(log.tlo_tx, '') || ' ' || COALESCE(log.log_no, '')
-       ELSE
-       COALESCE(log.log_no, '')
-     END AS ENDERECO,
-      COALESCE(bai.bai_no, '') AS BAIRRO,
-   loc.loc_no AS CIDADE,
-     loc.ufe_sg AS UF
-     FROM log_localidade loc
-     LEFT JOIN log_logradouro log ON log.loc_nu = loc.loc_nu
-     LEFT JOIN log_bairro bai ON log.bai_nu_ini = bai.bai_nu
+SELECT
+    log_logradouro.cep AS cep,
+    CONCAT(log_logradouro.tlo_tx, ' ', log_logradouro.log_no) AS endereco,
+    log_bairro.bai_no AS bairro,
+    log_localidade.loc_no AS cidade,
+    log_logradouro.ufe_sg AS uf,
+    log_logradouro.log_complemento AS complemento,
+    '' AS nome
+FROM
+    log_logradouro,
+    log_localidade,
+    log_bairro
+WHERE
+    log_logradouro.loc_nu = log_localidade.loc_nu
+    AND log_logradouro.bai_nu_ini = log_bairro.bai_nu
+    AND log_logradouro.log_sta_tlo = 'S'
 
-     UNION ALL
+UNION
 
-     SELECT uni.cep AS CEP,
-       uni.uop_endereco AS ENDERECO,
-    COALESCE(bai.bai_no, '') AS BAIRRO,
-      loc.loc_no AS CIDADE,
-   uni.ufe_sg AS UF
-   FROM log_unid_oper uni
-   LEFT JOIN log_bairro bai ON uni.bai_nu = bai.bai_nu
-   LEFT JOIN log_localidade loc ON uni.loc_nu = loc.loc_nu
+SELECT
+    log_logradouro.cep AS cep,
+    log_logradouro.log_no AS endereco,
+    log_bairro.bai_no AS bairro,
+    log_localidade.loc_no AS cidade,
+    log_logradouro.ufe_sg AS uf,
+    log_logradouro.log_complemento AS complemento,
+    '' AS nome
+FROM
+    log_logradouro,
+    log_localidade,
+    log_bairro
+WHERE
+    log_logradouro.loc_nu = log_localidade.loc_nu
+    AND log_logradouro.bai_nu_ini = log_bairro.bai_nu
+    AND log_logradouro.log_sta_tlo = 'N'
 
-   UNION ALL
+UNION
 
-   SELECT gra.cep AS CEP,
-     gra.gru_endereco AS ENDERECO,
-       COALESCE(bai.bai_no, '') AS BAIRRO,
-    loc.loc_no AS CIDADE,
-      gra.ufe_sg AS UF
-      FROM log_grande_usuario gra
-      LEFT JOIN log_bairro bai ON gra.bai_nu = bai.bai_nu
-      LEFT JOIN log_localidade loc ON gra.loc_nu = loc.loc_nu")
+SELECT
+    loc.cep AS cep,
+    '' AS endereco,
+    '' AS bairro,
+    loc.loc_no AS cidade,
+    loc.ufe_sg AS uf,
+    '' AS complemento,
+    '' AS nome
+FROM
+    log_localidade AS loc
+WHERE
+    loc.cep IS NOT NULL
+    AND loc.loc_nu_sub IS NULL
+
+UNION
+
+SELECT
+    loc.cep AS cep,
+    '' AS endereco,
+    loc.loc_no AS bairro,
+    locsub.loc_no AS cidade,
+    loc.ufe_sg AS uf,
+    '' AS complemento,
+    '' AS nome
+FROM
+    log_localidade AS loc,
+    log_localidade AS locsub
+WHERE
+    loc.cep IS NOT NULL
+    AND loc.loc_nu_sub IS NOT NULL
+    AND loc.loc_nu_sub = locsub.loc_nu
+
+UNION
+
+SELECT
+    log_cpc.cep AS cep,
+    log_cpc.cpc_endereco AS endereco,
+    '' AS bairro,
+    log_localidade.loc_no AS cidade,
+    log_cpc.ufe_sg AS uf,
+    '' AS complemento,
+    cpc_no AS nome
+FROM
+    log_cpc,
+    log_localidade
+WHERE
+    log_cpc.loc_nu = log_localidade.loc_nu
+
+UNION
+
+SELECT
+    log_grande_usuario.cep AS cep,
+    log_grande_usuario.gru_endereco AS endereco,
+    log_bairro.bai_no AS bairro,
+    log_localidade.loc_no AS cidade,
+    log_grande_usuario.ufe_sg AS uf,
+    '' AS complemento,
+    gru_no AS nome
+FROM
+    log_grande_usuario,
+    log_localidade,
+    log_bairro
+WHERE
+    log_grande_usuario.loc_nu = log_localidade.loc_nu
+    AND log_grande_usuario.bai_nu = log_bairro.bai_nu
+
+UNION
+
+SELECT
+    log_unid_oper.cep AS cep,
+    log_unid_oper.uop_endereco AS endereco,
+    log_bairro.bai_no AS bairro,
+    log_localidade.loc_no AS cidade,
+    log_unid_oper.ufe_sg AS uf,
+    '' AS complemento,
+    uop_no AS nome
+FROM
+    log_unid_oper,
+    log_localidade,
+    log_bairro
+WHERE
+    log_unid_oper.loc_nu = log_localidade.loc_nu
+    AND log_unid_oper.bai_nu = log_bairro.bai_nu")
 
 (defn create-cep-view
   []
   (jdbc/execute! ds [cep-view]))
+
+(defn fetch-ceps
+  []
+  #_(sql/find-by-keys ds :ceps :all {:order-by [:cep] :offset 0 :limit 10})
+  (plan/select! ds
+                (juxt :cep :endereco :bairro :cidade :uf :uf_nome)
+                ["SELECT
+                    *,
+                    CASE ceps.uf
+                      WHEN 'AC' THEN 'Acre'
+                      WHEN 'AL' THEN 'Alagoas'
+                      WHEN 'AP' THEN 'Amapá'
+                      WHEN 'AM' THEN 'Amazonas'
+                      WHEN 'BA' THEN 'Bahia'
+                      WHEN 'CE' THEN 'Ceará'
+                      WHEN 'DF' THEN 'Distrito Federal'
+                      WHEN 'ES' THEN 'Espírito Santo'
+                      WHEN 'GO' THEN 'Goiás'
+                      WHEN 'MA' THEN 'Maranhão'
+                      WHEN 'MT' THEN 'Mato Grosso'
+                      WHEN 'MS' THEN 'Mato Grosso do Sul'
+                      WHEN 'MG' THEN 'Minas Gerais'
+                      WHEN 'PA' THEN 'Pará'
+                      WHEN 'PB' THEN 'Paraíba'
+                      WHEN 'PR' THEN 'Paraná'
+                      WHEN 'PE' THEN 'Pernambuco'
+                      WHEN 'PI' THEN 'Piauí'
+                      WHEN 'RJ' THEN 'Rio de Janeiro'
+                      WHEN 'RN' THEN 'Rio Grande do Norte'
+                      WHEN 'RS' THEN 'Rio Grande do Sul'
+                      WHEN 'RO' THEN 'Rondônia'
+                      WHEN 'RR' THEN 'Rorâima'
+                      WHEN 'SC' THEN 'Santa Catarina'
+                      WHEN 'SP' THEN 'São Paulo'
+                      WHEN 'SE' THEN 'Sergipe'
+                      WHEN 'TO' THEN 'Tocantins'
+                    END AS uf_nome
+                 FROM ceps"]))
